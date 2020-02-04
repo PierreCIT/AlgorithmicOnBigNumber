926ef069734c6652b55228daec14c5d35aba65c9
noeim <noeim.jeux@gmail.com>
2020-02-04 11:35:53 +0100

    Substraction seems to be working

--- main.cpp
+++ main.cpp
@@ -5,24 +5,27 @@ using namespace std;
 
 int main() {
     cout << "Begins" << endl;
-    vector<uint64_t> initA = {4294967295 ,4294967295, 4294967295, 4294967295, 4294967295, 4294967295, 4294967295, 4294967295};
-    vector<uint64_t> initB = {0, 0, 0, 0, 0, 0, 0, 1};
-    vector<uint64_t> primary = {0, 0, 0, 0, 0, 0, 0, 17};
+    vector<uint64_t> initA = {1, 0, 0, 0, 0, 0, 0, 0};
+    vector<uint64_t> initB = {0, 1, 1, 1, 1, 1, 1, 1};
+    vector<uint64_t> primary = {2, 0, 0, 0, 0, 0, 0, 0};//{0, 0, 0, 0, 0, 999, 1700231273, 187355674};
     BigInt A(256, initA);
     BigInt B(256, initB);
     BigInt p(256, primary);
-    cout<< "A : ";
+    cout << "A : ";
     A.print();
-    cout<< "B : ";
+    cout << "B : ";
     B.print();
-    cout<< "p : ";
+    cout << "p : ";
     p.print();
-    cout << "A > B : "<<(A > B) << endl;
-    BigInt C = A+B;
+    cout << "A > B : " << (A > B) << endl;
+    BigInt C = A + B;
     cout << "A + B : ";
     C.print();
     cout << "A + B mod(p) : ";
-    C = A.addModular(B,p);
+    C = A.addModular(B, p);
+    C.print();
+    cout << "A - B mod(p) : ";
+    C = A.substract(B, p);
     C.print();
     return 0;
 }
